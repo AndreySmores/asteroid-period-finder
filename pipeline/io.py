696ac8209df9b15390c217e6d50e4af67b09e890
AndreySmores <asm092805@gmail.com>
2026-05-01 15:03:43 -0400

    io.py stubs and tests

---
+++ pipeline/io.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+
+def lightcurve_reader(filepath):
+    """"""
+    try:
+        path = Path(filepath)
+        extension = path.suffix
+    except Exception as e:
+        print(f"Invalid filepath: {filepath}! \nPlease retry with a valid path.\nException: {e}\n")
+        return None, -1
+    
+    match str(extension):
+        case '.xml': 
+            data, exit_status = xml_handler(path)
+        case '.json':
+            data, exit_status = json_handler(path)
+        case '.csv':
+            data, exit_status = csv_handler(path)
+        case ".tab":
+            data, exit_status = tab_handler(path)
+        case _:
+            print(f'Extension: {extension} of file {filepath} is not valid! \nPlease enter a valid filetype.\n')
+            return None, -1
+        
+    if exit_status < 0:
+        print(f'Reading of file: {filepath} failed.')
+        return None, -1
+    
+    return data, 0
+
+def xml_handler(path):
+    return 100, -1
+
+def json_handler(path):
+    return 200, 0
+
+def csv_handler(path):
+    return 300, 0
+
+def tab_handler(path):
+    return 400, 0
+
+def create_file_list(folder_path, extension):
+    
